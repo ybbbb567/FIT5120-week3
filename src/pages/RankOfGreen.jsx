2a25c78c3842b7f802f180234a900de1cb3954c5
yxuu0152 <yxuu0152@student.monash.edu>
2023-03-17 03:03:59 +1100

    RankofGardenWasteBin page add differention page

--- src/pages/RankOfGreen.jsx
+++ src/pages/RankOfGreen.jsx
@@ -15,6 +15,10 @@ class RankOfGreen extends Component {
     loading: false,
     productList: [],
     category: '',
+    pageNum: 1,
+    pageSize: 5,
+    total: 0,
+    productType: "productName",
     productValue: "",
   }
 
@@ -45,7 +49,7 @@ class RankOfGreen extends Component {
 
   render () {
 
-    const { loading, productList } = this.state
+    const { loading, productList, total, productValue, pageSize, productType } = this.state
     return (
       <div className="flex flex-col min-h-screen overflow-hidden">
         {/*  Site header */}
@@ -53,15 +57,33 @@ class RankOfGreen extends Component {
 
         {/*  Page content */}
         <main className="growgrow" color='#34fc16'>
-
+          
           <section className="relative">
             <div className="pt-32 pb-12 md:pt-40 md:pb-20">
               <div className="max-w-6xl mx-auto px-4 sm:px-6">
                 <div className="py-12 md:py-20 border-t border-gray-800">
                   <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
-                        <h2 className="h2 mb-4 text-black">Rank of Garden Waste</h2>
-                      </div>
-                  <Table bordered loading={loading} dataSource={productList} rowKey="_id">
+                    <h2 className="h2 mb-4 text-black">Rank of Garden Waste</h2>
+                    </div>
+                  <Table bordered pagination={{
+                    defaultPageSize: pageSize,
+                    showQuickJumper: true,
+                    showSizeChanger: true,
+                    pageSizeOptions: [5, 10, 15, 20],
+                    total,
+                    /*
+                    * 页码或 pageSize 改变的回调，参数是改变后的页码及每页条数
+                     */
+                    onChange: (pageNum, pageSize) => {
+                      this.setState({ pageNum }, () => {
+                        if (productValue !== "") {
+                          console.log('1')
+                        } else {
+                          console.log('12')
+                        }
+                      })
+                    }
+                  }} loading={loading} dataSource={productList} rowKey="_id">
                     <Column align={"center"} title="Name" dataIndex="name" key="name" />
                     <Column align={"center"} title="Category" dataIndex="category" key="category" />
                     <Column align={"center"} title="Degradation Time(Years)" dataIndex="degradation" key="degradation" sorter={true} defaultSortOrder='ascend' />
