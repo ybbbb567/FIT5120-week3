f31eab1b23a4b9ea391cdbb1e2548dc3920b146b
Lambert Li <ylii0159@student.monash.edu>
2023-03-16 15:17:08 +1100

    Add purple bin

--- src/partials/Bin.jsx
+++ src/partials/Bin.jsx
@@ -14,7 +14,7 @@ function Bin () {
         <h2 className="h2 mb-4 text-black">Waste Category</h2>
           </div>
           {/* Different color bins */}
-          <div className="max-w-sm mx-auto grid gap-8 lg:grid-cols-3 lg:gap-6 items-start lg:max-w-none">
+          <div className="max-w-sm mx-auto grid gap-8 lg:grid-cols-4 lg:gap-6 items-start lg:max-w-none">
 
             {/* Garden Waste Bin */}
             <div className="flex flex-col h-full p-6 bg-green-400" data-aos="fade-up">
@@ -25,7 +25,7 @@ function Bin () {
               </div>
               <blockquote className="text-base text-black grow">Your food and garden waste bin(green lid) is for food scraps, garden prunings and some organic material. The bin is collected every week.</blockquote>
               <div className="text-gray-700 font-medium mt-6 pt-5 border-t border-gray-700">
-                <Link to="/rankOfGreen" className="text-black-600 hover:text-gray-200 transition duration-150 ease-in-out">See the rank of Garden Waste</Link>
+                <Link to="/rankOfGreen" className="italic font-bold text-black-600 hover:text-gray-200 transition duration-150 ease-in-out">See the rank of Garden Waste &gt;&gt; </Link>
               </div>
             </div>
 
@@ -38,7 +38,7 @@ function Bin () {
               </div>
               <blockquote className="text-base text-black grow">Your recycling bin (yellow lid) is for common household packaging items typically bought at a supermarket and found in your kitchen, bathroom or laundry.</blockquote>
               <div className="text-gray-700 font-medium mt-6 pt-5 border-t border-gray-700">
-                <Link to="/rankOfYellow" className="text-black-600 hover:text-gray-200 transition duration-150 ease-in-out">See the rank of Recycling</Link>
+                <Link to="/rankOfYellow" className="italic font-bold text-black-600 hover:text-gray-200 transition duration-150 ease-in-out">See the rank of Recycling &gt;&gt;</Link>
               </div>
             </div>
 
@@ -51,7 +51,20 @@ function Bin () {
               </div>
               <blockquote className="text-base text-black grow">Your landfill bin(red lid) is for any items that cannot be recycled through your recycling bin, food and garden waste bin or other recycling services.</blockquote>
               <div className="text-gray-700 font-medium mt-6 pt-5 border-t border-gray-700">
-                <Link to="/rankOfRed" className="italic font-bold text-black-600 hover:text-gray-200 transition duration-150 ease-in-out">See the rank of Landfill</Link>
+                <Link to="/rankOfRed" className="italic font-bold text-black-600 hover:text-gray-200 transition duration-150 ease-in-out">See the rank of Landfill   &gt;&gt;</Link>
+              </div>
+            </div>
+
+    {/* Purple Bin */}
+    <div className="flex flex-col h-full p-6 bg-purple-400" data-aos="fade-up">
+              <div>
+                <div className="relative inline-flex flex-col mb-4">
+                  <span className="text-lg text-black grow"><b>Glass containers</b></span>
+                </div>
+              </div>
+              <blockquote className="text-base text-black grow">Your Glass containers bin(purple lid) is for glass bottles and jars.However, not all glass can be recycled. Drinking glasses and other household glass should all go in your rubbish bin. Only clean glass bottles and jars, without lids, are accepted.</blockquote>
+              <div className="text-gray-700 font-medium mt-6 pt-5 border-t border-gray-700">
+                <Link to="/rankOfRed" className="italic font-bold text-black-600 hover:text-gray-200 transition duration-150 ease-in-out">See the rank of Glass containers &gt;&gt;</Link>
               </div>
             </div>
 
