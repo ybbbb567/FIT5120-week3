3328439aa1b927d1291d3d681ce9374415df61be
ybbbb567 <985422523@qq.com>
2023-03-16 20:55:06 +1100

    try

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 
 import Header from '../partials/Header';
 import HeroHome from '../partials/HeroHome';
-import FeaturesBlocks from '../partials/FeaturesBlocks';
 import FeaturesZigZag from '../partials/FeaturesZigzag';
 import Footer from '../partials/Footer';
 
